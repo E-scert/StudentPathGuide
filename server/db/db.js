f35b07c8fdee0baf18b7226bfd2f5793bfd9d7cf
JR Escert <uhoneprincemulaudzi@gmail.com>
2026-03-15 18:12:36 +0200

    fix: add SSL to database connection for railway

--- server/db/db.js
+++ server/db/db.js
@@ -1,20 +1,17 @@
 import pg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const { Pool } = pg;
+
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "studentpathguide",
-  password: "prince",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool
   .connect()
   .then(() => console.log("Database connected successfully"))
-  .catch((err) => console.error("Database connection error,", err));
+  .catch((err) => console.error("Database connection error", err));
 
 export default pool;
